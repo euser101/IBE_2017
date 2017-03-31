1233434d2386d4c724b64a39333ed58576c6d6ce
euser101 <eric.studer@lernende.bfo-vs.ch>
2017-03-31 16:02:50 +0200

    Modifications

--- Code/Backend/PHP/Validation/input_filter.php
+++ Code/Backend/PHP/Validation/input_filter.php
@@ -1,17 +1,86 @@
 <?php
 	require "config.php";
 	session_start();
-try  
- {  
+try {  
   
-      if(isset($_POST[""]))
-      {
-        $preis = $_POST["Preis"];
-        $ort = $_POST["Ort"];
-        $land = $_POST["Land"];
-        $hName = $_POST["Hotelname"];
 
-        if(!isset($preis, $ort, $land, $hName) {//Dynamic SQL
+      if(isset($_POST["search"]))
+      {
+        $preis = $_POST["range_p"];
+        //$ort = $_POST["Ort"];
+        $land = $_POST["reise_z"];
+        //$hName = $_POST["Hotelname"];
+       
+
+        if(!isset($preis, $ort, $land, $hName)){ //Dynamic SQL
+
+           if (isset($land)) {
+
+              $sqlFrom[] = " tbl_land";
+              $sqlSelect[] = " Name_Land";
+              $sql[] = " Name_Land = '$land' ";
+              
+            }
+           if (isset($hName)) {
+
+              $sqlFrom[] = " tbl_hotel";
+              $sqlSelect[] = " Name_Hotel";
+              $sql[] = " Name_Hotel = '$hName' ";
+            }
+            if (isset($ort)) {
+
+              $sqlFrom[] = " tbl_ort";
+              $sql[] = " Name_ort = '$ort' ";
+            }
+            if (isset($preis)) {
+
+              $sqlForm[] = " tbl_zimmer";
+              $sqlSelect[] = " Preis";
+              $sql[] = " Preis = '$preis' ";
+            }
+          //Final Query
+        if (!empty($sql)) {
+
+            if($sqlSelect > 1){
+              $sqlSelect = implode(", ", $sqlSelect);
+              //FIX: Last element doesnt need a comma
+            }
+            if($sqlFrom > 1){
+              $sqlFrom = implode(", ", $sqlFrom);
+            }
+
+            //Converting the arrays to string so the query can use it
+            /*serialize($sqlSelect);
+            serialize($sqlFrom);*/
+
+            //FIX THIS
+            //$query = "SELECT $sqlSelect FROM $sqlFrom WHERE Name_Land = Schweiz";
+            $query = "SELECT * FROM `tbl_land` WHERE Name_Land = Schweiz";
+
+            /*
+            $query = "SELECT $sqlSelect FROM $sqlFrom 'WHERE ' . implode(' AND ', $sql)";*/
+
+            $stmt = $connect->prepare($query);//QUERY mit OR/LIKE
+                    $stmt->execute();
+
+            
+            foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $row) {
+              print_r($row);
+
+            }
+          }
+        }
+
+      }
+      else{
+          $errMsg = "Please input something to search for.";
+      } 
+    }
+catch(PDOException $error) {  
+  $errMsg = $error->getMessage();  
+}  
+        /*
+        if(!isset($preis, $ort, $land, $hName){//Dynamic SQL
             unset($sql);
             //http://stackoverflow.com/questions/15794179/create-a-dynamic-mysql-query-using-php-variables
             //example query: User inputs land and ort, SELECT $preis, $ort FROM tbl_Land, tbl_ort
@@ -49,15 +118,16 @@ try
               $query = "SELECT $sqlSelect FROM $sqlFrom 'WHERE ' . implode(' AND ', $sql)";
               $stmt = $connect->prepare($query);//QUERY mit OR/LIKE
                       $stmt->execute();
-            }
-            
-        }  else{
-          $errMsg = "Please input something to search for.";
-        }
 
+            foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $row) {
+              print_r($row);
+            }
+          }
+            
+        }  
   
-catch(PDOException $error)  
- {  
-      $errMsg = $error->getMessage();  
- }  
+        } 
+      }*/
+      
+
 ?>
