a4dfde1461b5863088f5dda623adef728e2eb9f3
euser101 <eric.studer@lernende.bfo-vs.ch>
2017-06-09 13:51:34 +0200

    Add files via upload

--- input_filter.php
+++ input_filter.php
@@ -4,23 +4,13 @@
 try {  
       //HTTP doc timeout fix
       header("Cache-Control: max-age=600");
-      if(isset($_POST["search"])) 
-      /*TODO:
-      //Make sure that html slider defaults values to 0 or not specified (Advanced Filter)
-      Adjust the multidimensional array, so that only hotels that match to a specific country get listed and not all of them*/
-      //Sanitize user input, restrict to couple of characters, fix results and empty info
-      //FIX Directory structure
-      {
+      if(isset($_POST["search"])) {
         //Declaring vars
         $landHotelOrt = $_POST["reise_z"];
         $preis = $_POST["range_p"];
         $anr = $_POST["anreise"];
         $abr = $_POST["abreise"];
         
-      
-        /*$zimmer = $_POST["zimmer"];
-        $erw = $_POST["erwachsene"];
-        $kin = $_POST["kinder"];*/
         //Filtering out the Query to get dynamic SQL
         if( isset($landHotelOrt) || isset($preis) || isset($anr) || isset($abr) || isset($zimmer) || isset($erw) || isset($kin) ){ 
           
@@ -55,8 +45,7 @@ try {
                 $tbl_name[] = ", tbl_hotel";
               }else{
                 $tbl_name[] = "tbl_hotel";
-              }
-             
+              }             
             }           
 
             if ($zimmer > 0) {      
@@ -71,8 +60,7 @@ try {
                   $tbl_name[] = ", tbl_hotel";
                 }else{
                   $tbl_name[] = "tbl_hotel";
-                }
-                
+                }              
               } 
     
             }else{
@@ -126,28 +114,223 @@ try {
 
         $stmt0->execute();
         $stmt1->execute();
-        /*Deeebuggin
-        echo "<br>";
-        echo $mainSQL;
-        echo "<br>";
-        echo $countSQL;
-        //echo "Query: " .$mainSQL;
-        //echo "<br>";
-        //echo "Anz Erg: " .count($result1);
-        //var_dump($result1);
-        */
 
         $result0 = $stmt0->fetchAll();
         $result1 = $stmt1->fetchAll();
 
-        $count = $result0[0][0];//How many results match?
+        $count = $result0[0][0]; //How many results match?
+        $realVal = count($result1); //Count results
+        
+        $errBtn = "<span class='closebtn'>&times;</span>";
 
-        $realVal = count($result1);
-    
+        $startHTML = " <!doctype html>
+            <html lang='de'>
+              <head>
+                <meta charset='utf-8'>
+                <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+                 <link href='CSS/stylesheet.css' rel='stylesheet'>
+                <link href='CSS/stylesheet.css' rel='stylesheet'>
+               <script src='JS/copyright_update.js'></script>
+               <script src='JS/sort.js'></script>
+               <link href='stylesheet.css' rel='stylesheet'>
+                <title>Suche</title>
+
+                <!--<link rel='import' href='navbar.html'>-->
+
+
+              </head>
+              <body>
+              
+              
+                  <ul class='topnav' id='myTopnav'>
+                  <li><a href='Home.html'><img style ='padding-top: 10px;' class='logo' src='..\img/Logo/logo.png' alt='home'/></a></li>
+                  <li><a style ='padding-top: 15px; padding-bottom: 15px;' href='Top10.html'>Top 10</a></li>
+                  <li><a style ='padding-top: 15px; padding-bottom: 15px;' href='AGB.html'>AGB</a></li>
+                  <li><a style ='padding-top: 15px; padding-bottom: 15px;' href='#hilfe'>Hilfe</a></li>
+                  <li><a style ='padding-top: 15px; padding-bottom: 15px;' href='#konto'>Konto</a></li>
+                  <li class='icon'>
+                  <a href='javascript:void(0);' onclick='myFunction()'>&#9776;</a>
+                  </li>
+                </ul>
+              <script>
+                    /* Toggle between adding and removing the 'responsive' class to topnav when the user clicks on the icon */
+                    function myFunction() {
+                      var x = document.getElementById('myTopnav');
+                      if (x.className === 'topnav') {
+                          x.className += ' responsive';
+                      } else {
+                          x.className = 'topnav';
+                      }
+                  }                
+               </script>
+            <style>
+            /* Remove margins and padding from the list, and add a black background color */
+            ul.topnav {
+                list-style-type: none;
+                margin: 0;
+                padding: 2px;
+                overflow: hidden;
+                background-color: #D8D8D8;
+            }
+            /* Float the list items side by side */
+            ul.topnav li {float: left;}
+            /* Style the links inside the list items */
+            ul.topnav li a {
+                display: inline-block;
+                color: #2E2E2E;
+                text-align: center;
+                padding: 14px 16px;
+                text-decoration: none;
+                transition: 0.3s;
+                font-size: 23px;
+              font-family: Arial;
+              font-style: italic;
+            }
+            /* Change background color of links on hover */
+            ul.topnav li a:hover {background-color: #77AFE1; color:#000000; }
+            /* Hide the list item that contains the link that should open and close the topnav on small screens */
+            ul.topnav li.icon {display: none;}
+            /* When the screen is less than 680 pixels wide, hide all list items, except for the first one ('Home'). Show the list item that contains the link to open and close the topnav (li.icon) */
+            @media screen and (max-width:680px) {
+              ul.topnav li:not(:first-child) {display: none;}
+              ul.topnav li.icon {
+                float: right;
+                display: inline-block;
+              }
+            }
+            /* The 'responsive' class is added to the topnav with JavaScript when the user clicks on the icon. This class makes the topnav look good on small screens */
+            @media screen and (max-width:680px) {
+              ul.topnav.responsive {position: relative;}
+              ul.topnav.responsive li.icon {
+                position: absolute;
+                right: 0;
+                top: 0;
+              }
+              ul.topnav.responsive li {
+                float: none;
+                display: inline;
+              }
+              ul.topnav.responsive li a {
+                display: block;
+                text-align: left;
+              }
+            }
+            /*Styling alert button*/
+            .alert {
+                padding: 20px;
+                background-color: #f44336;
+                color: white;
+                opacity: 1;
+                transition: opacity 0.6s;
+                margin-bottom: 15px;
+            }
+
+            .alert.success {background-color: #4CAF50;}
+            .alert.info {background-color: #2196F3;}
+            .alert.warning {background-color: #ff9800;}
+
+            .closebtn {
+                margin-left: 15px;
+                color: white;
+                font-weight: bold;
+                float: right;
+                font-size: 22px;
+                line-height: 20px;
+                cursor: pointer;
+                transition: 0.3s;
+            }
+
+            .closebtn:hover {
+                color: black;
+            }
+
+          </style>
+              
+            <div id='Suche'>
+              <!--Eingabe Reiseziel-->
+            <form method='POST' class='Suchkriterien' action=''>
+                Reiseziel<br>
+                <input class='Sucheinput' type='text' name='reise_z' placeholder='Land, Hotelname'>
+                </form>
+                
+                <!--Preis angabe-->
+                Preis<br>
+                <input class='Suchkriterien' name='range_p' id='slider1' type='range' min='100' max='500' step='10' />
+                
+                <!--An- und Abreise-->
+                <br>
+                <br>
+                <label for='gebdat'>
+                  Anreise<br>
+                  <input type='date' id='gebdat' name='anreise'>
+                </label>
+                
+                <br>
+                <label for='gebdat'>
+                  Abreise<br> 
+                  <input type='date' id='gebdat' name='abreise'>
+                </label>
+                <p><br></p>
+                
+                <!--Knopf Home-->
+                <a href='#display more input types'>  <button style='width: 150px; vertical-align:middle;' class='buttonSuche'><span>Mehr Optionen </span></button></a>
+                
+                <!--Knopf Suche-->
+                <button class='buttonSuche' name='search' style='width: 150px; vertical-align:middle;'><span>Suche </span></button>
+
+
+                <div id='Sortieren'>
+                  <p>Sortieren nach:</p>
+                  <button class='buttonSortieren' onclick='srtName()'>Name</button>
+                  <button class='buttonSortieren' onclick='srtPrice()'>Preis</button>
+                  <button class='buttonSortieren' onclick='srtReview()'>Bewertung</button>
+                </div>
+              </div> 
+            </form>";
+
+        $endHTML = "<footer>
+            <article>
+            &copy <script>refreshCopyright();</script> IBE_2017 
+            <br>
+            Website is under construction <br>Change Language | <a href='help.html'>Help</a>
+            </article>
+          </footer>
+          <style>
+          /*Styling the footer*/
+          footer {
+            position: scroll;
+            display: block;
+            right: 0;
+            left: 0;
+            bottom: 0;
+            text-align: center;
+            background-color: #1D4D77;
+            padding: 2.5%;
+            width: 100%;
+            margin-top: 5%;
+
+          }
+          /*End of footerstyle*/
+          </style>
+          </html>";
+
           //Displaying the result as a table if result isnt empty
           if ($realVal == 0){// | empty($result2) | empty($result3)
-            $errMsg = "No Results were found. Please check your input.";      
-            echo $errMsg;   
+            $errMsg = "Please check your input.";            
+           
+          //Start HTML
+          echo $startHTML;
+
+            //Format error message
+            echo "<div class='alert'>";
+
+              echo "<strong>No Results were found! </strong> $errMsg";
+              echo "</div>";   
+
+            
+
+          //End HTML
+          echo $endHTML;
           }else{
 
               if ($count > 0) {
@@ -157,24 +340,54 @@ try {
               }
             $row = 0;
             $path = "img/hotels/";
-            for ($i; $i <= $count; $i++) { 
+          
+          //Start HTML
+          echo $startHTML;
 
-              //echo "<img src='".$result1[$row][0]."'>";
-              echo "<img src='".$path.$result1[$row][0]."'>";
-              echo "<ul>";
-              for ($col = 1; $col <= 4; $col++) {
-                  echo "<li>".$result1[$row][$col]."</li>";
-              }
-              echo "</ul>";
-              //Getting the hotelname and saving it to the url for next query
-              $hname = serialize($result1[$row][1]);
-              echo "<form method='post' action='filter_detail.php?hName=$hname' name='adv_search'><input type='submit' name='hotelname' value='More Information'></form>";           
-            $row++;
-            } 
+            //Format count of found query
+            if (isset($landHotelOrt)) {
+              echo "<div class='alert success'>";
+              echo "<strong>$count</strong>";
+              if ($count > 1) {
+                echo " Resultate für: ".$landHotelOrt."";
+              }else{
+                echo " Resultat für: ".$landHotelOrt."";
+              } 
+              echo "</div>";   
+              echo "<br>"; 
+              echo "<br>"; 
+            }
+    
+            for ($i; $i <= $count; $i++) { 
+                          //echo "<img src='".$result1[$row][0]."'>";
+                          echo "<div id='ListeHotel'>";
+                            echo "<div class='Hotel'>";
+                            echo "<img class='bild_liste' src='".$path.$result1[$row][0]."'>";       
+                            echo "<h2>".$result1[$row][1]."</h2>";
+                            echo "<p>".$result1[$row][2]." Sterne</p>";  
+
+                           if ($result1[$row][3] > 1) {
+                              echo "<p>".$result1[$row][3]." Bewertungen</p>";
+                            }else{
+                              echo "<p>".$result1[$row][3]." Bewertung</p>";
+                            }
+                            
+                            echo "<p class='price'>".$result1[$row][4]." CHF</p>";
+
+                          $hname = serialize($result1[$row][1]);
+                            echo "<form method='post' action='filter_detail.php?hName=$hname' name='adv_search'><input type='submit' name='hotelname' value='Mehr Infos'></form>";   
+                          echo "</div>";
+                          //Getting the hotelname and saving it to the url for next query
+                                  
+                        $row++;
+                        echo "</div>";
+              }         
+    
+          //End HTML
+          echo $endHTML;
 
         }
-
-      
+    
       }
       else{
           $errMsg = "Please input something to search for.";
@@ -183,5 +396,5 @@ try {
   }
 catch(PDOException $error) {  
   $errMsg = $error->getMessage();  
-}  
+}
 ?>
