04c7d5fe39b9a728920dfb61c2ebd6c010a75cab
euser101 <eric.studer@lernende.bfo-vs.ch>
2017-06-09 21:30:32 +0200

    Add files via upload

--- filter_detail.php
+++ filter_detail.php
@@ -1,14 +1,11 @@
 <?php   
   require "config.php";
-	try {
+  try {
     //http timeout fix
     header("Cache-Control: max-age=600");
     $hName = unserialize($_GET["hName"]);
-		$sql = "SELECT Name_Hotel, Preis, Sterne, Bewertung, Beschreibung, Bild, Location FROM tbl_hotel, tbl_ort, tbl_bild WHERE Name_Hotel='$hName' AND FK_Hotel = PK_Hotel";
+    $sql = "SELECT Name_Hotel, Preis, Sterne, Bewertung, Beschreibung, Bild, Location FROM tbl_hotel, tbl_ort, tbl_bild WHERE Name_Hotel='$hName' AND FK_Hotel = PK_Hotel AND PK_Ort = FK_Ort";
     
-    
-
-
         $stmt = $connect->prepare($sql);
         $stmt->execute();
         $result = $stmt->fetchAll();
@@ -36,80 +33,113 @@
             <meta name='viewport' content='width=device-width, initial-scale=1'>
               <link rel='stylesheet' href='CSS/frontend_style.css'>
               <link href='CSS/stylesheet.css' rel='stylesheet'>
-            <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
+              <script src='JS/copyright_update.js'></script>
+              <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
           </head>
-          <body id='start'>          
+          <body id='start'>         
           ";
           echo $html1;
           include('HTML/navbar.html');
-          echo $sql;
-          $html2 = "
-
-          ";
-
-
-          $slide1 = "path";
-
-          /*<div class="slideshow-container">
-
-          <div class="mySlides fade">
-            <div class="numbertext">1 / 3</div>
-            <img src="../img/Schweiz/BaurauLac/Hotel1_Bild1_ch.jpg" style="width:100%">
-            <div class="text">Caption Text</div>
-          </div>
-
-          <div class="mySlides fade">
-            <div class="numbertext">2 / 3</div>
-            <img src="../img/Schweiz/BaurauLac/Hotel1_Bild2_ch.jpg" style="width:100%">
-            <div class="text">Caption Two</div>
-          </div>
-
-          <div class="mySlides fade">
-            <div class="numbertext">3 / 3</div>
-            <img src="../img/Schweiz/BaurauLac/Hotel1_Bild3_ch.jpg" style="width:100%">
-            <div class="text">Caption Three</div>
-          </div>
-
-          <a class="prev" onclick="plusSlides(-1)">?</a>
-          <a class="next" onclick="plusSlides(1)">?</a>
-
-          </div>*/
-
-          //Preparing pics for slideshow
-
-
-
+          echo "<br>";
+          echo "<br>";
+          echo "<div id='ListeHotel'>";
           $path = "img/hotels/";
-          for ($row = 0; $row < $x; $row++) {
-            //echo "<p><b>Row number $row</b></p>";
-            echo "<img src='".$path.$result[$row][5]."'>";
 
-            echo "<iframe id='Top10map' src='".$result[$row][6]."'frameborder='0 style='border:0' allowfullscreen target='_blank'></iframe>";
-            echo "<ul>";
-            
-            for ($col = 0; $col < 5; $col++) {
-              echo "<p>".$result[$row][$col]."</p>";
-           	  echo "</ul>";
-			      }
+          echo "<div class='HotelName'</div>";
 
-            $starNumber = implode(array($result[$row][2]));
+          echo "<div style='text-align: center'</div>";
+          echo "<h3>".$result[0][0]."</h3>";
+          $starNumber = implode(array($result[0][2]));
+
             if ($starNumber < 5) {
               for($y=1;$y<=$starNumber;$y++) {
-                    echo '<img src="img/stars/star_full.png" style="width:5%;"/>';
+                    echo '<img src="img/stars/star_full.png" style="width:3%;"/>';
                 }
                 if (strpos($starNumber,'.')) {
-                    echo '<img src="img/stars/star_full.png" style="width:5%;"/>';
+                    echo '<img src="img/stars/star_full.png" style="width:3%;"/>';
                     $y++;
                 }
                 while ($y<=5){
-                    echo '<img src="img/stars/star_part.png" style="width:5%;"/>';
+                    echo '<img src="img/stars/star_part.png" style="width:3%;"/>';
                     $y++;
                 }
 
           }
-     			 
-      }
+          echo "</div>";
+          echo "<br>";
+          echo "</div>";
 
+          /*
+          Slideshow
+          */
+          
+          echo "<div class='slideshow-container'</div>";
+          for ($i=0; $i < 3; $i++) {      
+            echo "<div class='mySlides fade'>";
+              echo "<div class='numbertext'>$i / 2</div>";
+              echo "<img src='".$path.$result[$i][5]."' style='width:100%'>";
+              echo "<div class='text'>".$result[0][0]."</div>";
+            echo "</div>";
+
+          }  
+
+          $html2 = "
+            <a class='prev' onclick='plusSlides(-1)'>?</a>
+            <a class='next' onclick='plusSlides(1)'>?</a>
+
+            </div>
+            <br>
+
+            <div style='text-align:center'>
+              <span class='dot' onclick='currentSlide(1)'></span> 
+              <span class='dot' onclick='currentSlide(2)'></span> 
+              <span class='dot' onclick='currentSlide(3)'></span> 
+            </div>
+            <script> 
+
+            </script>
+            <script src='JS/Bewertungen.js'></script>
+            <script src='JS/Hotelfeed.js'></script>
+
+            </br>
+
+            <div id='flip'>Für Beschreibung und Bewertungen &#x2192; klicken</div>
+
+            <div id='panel'>
+            <p>";
+            $html3 = "
+            Baur au Lac heißt Booking.com-Gäste seit 14. Dez. 2009 willkommen.</br>
+            </br>";
+            $html4 = "(Durchschnittliche Bewertung)
+             
+              </br>
+              </br>
+               Eigene Bewetung abgeben: <style= 'color:white'>
+              </p>
+              </div>
+          ";   
+          
+          echo $html2;
+          echo $result[0][4];
+          echo $html3;
+         //echo $result[0][3];
+
+          $starNumber = implode(array($result[0][3]));
+            if ($starNumber < 5) {
+              for($y=1;$y<=$starNumber;$y++) {
+                    echo '<img src="img/stars/star_full.png" style="width:3%;"/>';
+                }
+                if (strpos($starNumber,'.')) {
+                    echo '<img src="img/stars/star_full.png" style="width:3%;"/>';
+                    $y++;
+                }
+                while ($y<=5){
+                    echo '<img src="img/stars/star_part.png" style="width:3%;"/>';
+                    $y++;
+                }
+
+          }
+          echo $html4;
     }
 
 }
